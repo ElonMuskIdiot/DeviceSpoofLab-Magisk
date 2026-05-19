bbcf0fc357042047b9d7e7e07d46c1ebfa8565c6
ElonMuskIdiot <167176018+ElonMuskIdiot@users.noreply.github.com>
2026-05-19 15:43:48 +0300

    'fix'

--- customize.sh
+++ customize.sh
@@ -5,7 +5,7 @@ SKIPUNZIP=0
 
 ui_print " "
 ui_print "********************************"
-ui_print "   DeviceSpoofLabs v2.3"
+ui_print "   DeviceSpoofLabs v2.3.1"
 ui_print "********************************"
 ui_print " "
 
@@ -30,7 +30,7 @@ set_permissions() {
     ui_print "- Setting permissions"
     set_perm_recursive "$MODPATH" 0 0 0755 0644
     set_perm_recursive "$MODPATH/common" 0 0 0755 0755
-        set_perm "$MODPATH/system/bin/devicespooflabs" 0 2000 0755
+	set_perm "$MODPATH/system/bin/devicespooflabs" 0 2000 0755
     set_perm "$MODPATH/system/bin/devicespooflabsnew" 0 2000 0755
     set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
     set_perm "$MODPATH/service.sh" 0 0 0755
